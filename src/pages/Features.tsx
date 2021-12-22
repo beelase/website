2ba487b5b18718676fc1cc1c4688b6e3e992f9f5
dendenmuniz <dendenmuniz@gmail.com>
2021-12-22 17:52:26 +0100

    fix few css issues

--- src/pages/Features.tsx
+++ src/pages/Features.tsx
@@ -19,7 +19,7 @@ export function Features() {
             <!-- Start features content --> */}
               <div className="mu-features-content">
                 <div className="row">
-                  <div className="col-lg-3 col-md-3  col-sm-12">
+                  <div className="col-lg-3 col-md-3  col-sm-6">
                     <div className="mu-single-feature p-4">
                       <span>
                         <img src={podcastImg} alt="podcast" />
